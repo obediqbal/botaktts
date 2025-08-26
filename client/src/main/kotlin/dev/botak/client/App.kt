ca5aceced940971fdaeebf94684136e199ed8e2d
Rezapu Obed <obed.ubaidillah@gmail.com>
2025-08-26 15:56:22 +0700

    chore: Remove unused SLF4J logger from `App.kt`
    Deleted the `LoggerFactory` import and the `LOGGER` instance, which were not referenced anywhere in the client module. This cleans up dead code and eliminates an unnecessary dependency on SLF4J for the application entry point.

--- client/src/main/kotlin/dev/botak/client/App.kt
+++ client/src/main/kotlin/dev/botak/client/App.kt
@@ -7,12 +7,10 @@ import dev.botak.client.windows.AppMainWindow
 import dev.botak.client.windows.SystemTrays
 import dev.botak.core.services.AudioStreamService
 import dev.botak.core.services.TTSService
-import org.slf4j.LoggerFactory
 
 private val ttsService by lazy { TTSService() }
 private val audioStreamService by lazy { AudioStreamService() }
 private val globalFocusRequester = FocusRequester()
-private val LOGGER = LoggerFactory.getLogger("dev.botak.client.App")
 
 fun start() =
     application {
